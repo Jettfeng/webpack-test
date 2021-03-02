0efa07b1a0c93d8a7967c871ff27279651137edc
lixiaofeng <1130034920@qq.com>
2021-03-02 22:32:06 +0800

    2021/03/02 shimming预置依赖=>shimming预置全局变量->全局Exports

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,17 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import { file,helpers } from './globals.js'
+function component() {
+  const element = document.createElement("div");
 
- function component() {
-   const element = document.createElement('div');
- 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   // lodash，现在通过一个 script 引入
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- 
-   return element;
- }
- 
- document.body.appendChild(component());
+  //  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(["Hello", "webpack"], " ");
+  // 假设我们处于 `window` 上下文
+  alert("Hmmm, this probably isn't a great idea...");
+  console.log(file);
+  helpers.parse()
+  return element;
+}
+
+document.body.appendChild(component());
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path')
-
+const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename:'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      // {
+      //   test: require.resolve('./src/index.js'),
+      //   use: 'imports-loader?wrapper=window',
+      // },
+      {
+        test: require.resolve('./src/globals.js'),
+        loader:"exports-loader",
+        options: {
+          type:'module',
+          exports: ['file','helpers']
+        }
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // _: 'lodash',
+      join: ['lodash', 'join'],
+    }),
+  ]
 }
